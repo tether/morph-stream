d0be38dc807c63089d0732f6bbcf6e24e1b0e759
bredele <olivier.wietrich@gmail.com>
2017-06-15 15:18:25 -0600

    Morph promise with object mode true

--- index.js
+++ index.js
@@ -11,11 +11,18 @@ module.exports = function (value) {
   const type = typeof value
   const bool = type === 'object'
   const stream = new Readable({
-    objectMode:bool
+    objectMode: bool
   })
   stream._read = () => {}
-  stream.push(bool ? value : value.toString())
-  stream.push(null)
+  if (bool && typeof value.then === 'function') {
+    value.then(reason => {
+      stream.push(reason)
+      stream.push(null)
+    })
+  } else {
+    stream.push(bool ? value : value.toString())
+    stream.push(null)  
+  }
   return stream
   // do something
 }
