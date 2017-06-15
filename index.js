554123a17d32440ad437fa1ee094c6172cc0a283
bredele <olivier.wietrich@gmail.com>
2017-06-15 15:07:44 -0600

    Morph string

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+const Readable = require('readable-stream').Readable
 
 /**
+
  * This is a simple description.
  *
  * @api public
  */
 
-module.exports = function () {
+module.exports = function (value) {
+  const stream = new Readable
+  stream._read = () => {}
+  stream.push(value)
+  stream.push(null)
+  return stream
   // do something
 }
-
-  
