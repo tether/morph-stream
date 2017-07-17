ca37dcad2e3ea759a3eb007cba2d8e43767b5620
bredele <olivier.wietrich@gmail.com>
2017-07-17 16:42:31 -0600

    should morph promise into stream

--- test/morph-stream.js
+++ test/morph-stream.js
@@ -6,9 +6,10 @@ const test = require('tape')
 const morph = require('..')
 const concat = require('concat-stream')
 
-test('morph string into stream', assert => {
+test('morph promise into stream', assert => {
   assert.plan(1)
-  morph('hello').pipe(concat(data => {
+  const promise = new Promise(resolve => resolve('hello'))
+  morph(promise).pipe(concat(data => {
     assert.equal(data.toString(), 'hello')
   }))
 })
