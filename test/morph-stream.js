c93b22ac12730cdcd4597882cce7a0bdd028f10c
bredele <olivier.wietrich@gmail.com>
2017-07-17 16:50:23 -0600

    should morph rejected promise

--- test/morph-stream.js
+++ test/morph-stream.js
@@ -6,10 +6,16 @@ const test = require('tape')
 const morph = require('..')
 const concat = require('concat-stream')
 
-test('morph promise into stream', assert => {
+test('morph resolved promise into stream', assert => {
   assert.plan(1)
   const promise = new Promise(resolve => resolve('hello'))
   morph(promise).pipe(concat(data => {
     assert.equal(data.toString(), 'hello')
   }))
+})
+
+test('morph rejected promise into stream', assert => {
+  assert.plan(1)
+  const promise = new Promise((resolve, reject) => reject('hello'))
+  morph(promise).on('error', err => assert.equal(err, 'hello'))
 })
